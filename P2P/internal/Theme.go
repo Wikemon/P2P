d52d6e1bfdfb76c12c761ee456143a77f0ce673f
Wikemon <panovartem2008@gmail.com>
2025-06-20 13:35:11 +0300

    Add files via upload

---
+++ P2P/internal/Theme.go
@@ -0,0 +1,9 @@
+package internal
+
+import "image/color"
+
+type ChatTheme struct {
+	PanelColor  color.Color
+	ChatBgColor color.Color
+	AvatarImage string
+}
---
+++ P2P/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"embed"
+	"testfyne/internal"
+)
+
+var assets embed.FS
+
+func main() {
+	internal.Ui()
+}
